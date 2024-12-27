20b9a740fc5771dc261bffbb22b34cb19cd82df9
gaeulzzang <gaeulzzang@gmail.com>
2024-12-27 20:22:04 +0900

    #5 [feat] SecurityConfig 추가

--- src/main/java/com/solux/sm137/infra/config/SecurityConfig.java
+++ src/main/java/com/solux/sm137/infra/config/SecurityConfig.java
@@ -1,46 +1,40 @@
-//package com.solux.sm137.infra.config;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-//
-//@RequiredArgsConstructor
-//@Configuration
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                // CSRF 설정
-//                .csrf(AbstractHttpConfigurer::disable)
-//
-//                // 세션 관리 설정
-//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//
-//                // 인증 실패 시 처리
-//                .exceptionHandling(exceptions -> exceptions
-//                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-//                )
-//
-//                // 요청 경로에 따른 인증/권한 설정
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .requestMatchers("/api/v1/user/modify").authenticated()
-//                        .anyRequest().permitAll()
-//                )
-//
-//                // OAuth2 리소스 서버 설정
-//                .oauth2ResourceServer(oauth2 -> oauth2
-//                        .jwt(jwt -> jwt
-//                                .jwkSetUri("https://your-auth-server.com/.well-known/jwks.json")
-//                        )
-//                );
-//        return http.build();
-//    }
-//
-//}
+package com.solux.sm137.infra.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+
+    private static final String[] ALLOWED_URIS = {
+            "/api/v1/user",
+            "/api/v1/user/modify",
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)  // CSRF 보호 비활성화
+                .cors(withDefaults()) // CORS 설정 추가
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers(ALLOWED_URIS).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // Stateless 설정
+                );
+
+        return http.build();
+    }
+
+}
