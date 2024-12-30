521e5823ba54c42ec45b5ea0264db79071d4a00e
Answl <118355919+Answl@users.noreply.github.com>
2024-12-31 04:15:10 +0900

    [feat] 회원탈퇴 api 구현

--- src/main/java/com/solux/sm137/infra/apiPayload/status/SuccessStatus.java
+++ src/main/java/com/solux/sm137/infra/apiPayload/status/SuccessStatus.java
@@ -13,7 +13,8 @@ public enum SuccessStatus implements BaseCode {
     _GET_USER_INFO_SUCCESS(200, "회원정보 조회에 성공하였습니다."),
     _GET_MY_COMPLAINTS_SUCCESS(200, "내 민원 리스트 조회에 성공하였습니다."),
     _GET_RESULTS_SUCCESS(200, "결과 조회에 성공하였습니다."),
-    _GET_SCRAPS_SUCCESS(200, "스크랩 리스트 조회에 성공하였습니다.")
+    _GET_SCRAPS_SUCCESS(200, "스크랩 리스트 조회에 성공하였습니다."),
+    _USER_DELETED(200,"회원탈퇴에 성공하였습니다.")
     ;
 
     private final int code;
