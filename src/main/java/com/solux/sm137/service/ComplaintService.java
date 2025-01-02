93597792cc255f165df2ddf95b10e2c70026bfe3
Answl <118355919+Answl@users.noreply.github.com>
2025-01-02 23:24:07 +0900

    [refactor] ComplaintService 수정

--- src/main/java/com/solux/sm137/service/ComplaintService.java
+++ src/main/java/com/solux/sm137/service/ComplaintService.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
-@Transactional
+@Transactional(readOnly=true)
 @RequiredArgsConstructor
 public class ComplaintService {
 
